5be003ad87c592879ce5f2426b2cc50d1dab18eb
Luis Felipe Mejia Castrillon <luisfe_617@outlook.com>
2017-08-25 10:33:11 -0500

    mongodb and nodejs ws

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const app = require('./app');
+const config = require('./config');
+
+mongoose.connect(config.db, function(err, res) {
+    if (err) {
+        return console.log(`Error al conectar a la base de datos ${err}`)
+    }
+    console.log("Conexion a la base de datos establecida");
+
+    app.listen(config.port, function() {
+        console.log(`API REST corriendo en puerto ${config.port}`);
+    })
+});
---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const services = require('../services');
+
+function isAuth(req, res, next) {
+    if(!req.headers.authorization){
+        return res.status(403).send({message: `No tienes autorización`})
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+    
+    services.decodeToken(token)
+    .then(function(response) {
+        req.user = response;
+        next();
+    })
+    .catch(function(response) {
+        res.status(response.status)
+    })
+}
+
+module.exports = isAuth;
